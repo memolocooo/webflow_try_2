390e76bd5684353b15b1c058e659215f1c754d6f
Guillermo Avalos <guillermo9811@gmail.com>
2025-02-06 19:51:19 -0600

    replace both

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import requests
 import os
-from videoseries import getOrders, getFees
 from dotenv import load_dotenv
 
 # Load environment variables from .env
